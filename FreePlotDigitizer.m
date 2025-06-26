e1b8ca1cc99a3f9b3f67598bb6ef876be99c5a93
BigFluffyBeard <shyane.cornell@outlook.com>
2025-06-25 23:45:10 -0700

    Update FreePlotDigitizer.m
    Made some...rather drastic changes. I changed how axis calibration is handled, and also changed how tracing in both the geometry and curve modes is handled. I'll update the READme accordingly.

--- FreePlotDigitizer.m
+++ FreePlotDigitizer.m
@@ -4,8 +4,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function FreePlotDigitizer()
-    % How do you want your data? This asks you how you want it output. For
-    % my purposes, I have curves and geometry formats. 
+    %%% How do you want your data? This asks you how you want it output. For
+    %%% my purposes, I have curves and geometry formats. 
     figUI = uifigure('Name', 'Welcome! What are you outputting?','Position',[100 100 350 150]);
     uilabel(figUI,'Position',[20 100 100 20], 'Text','Output:');
     ddMode = uidropdown(figUI,'Position',[120 100 180 20],'Items',{'Curve','Geometry'});
@@ -15,13 +15,13 @@ function FreePlotDigitizer()
     close(figUI);
 
 
-    % Now we need to get dat figure. I'll try to make it accepting of all file types
+    %%% Now we need to get dat figure. I'll try to make it accepting of all file types
     cancelCounter = 0;
     [fname,path] = uigetfile({'*.png;*.jpg;*.tif;*.mpeg;*.gif','Image Files'},'Pick ur graph!');
     while isequal(fname,0)  % Now don't you be hurtin my digitizer's feelings. Yes, it's 3 am. What's it to ya?
         cancelCounter = cancelCounter + 1;
         if cancelCounter == 2
-            msgbox('I"m starting to take this personally.')
+            msgbox("I'm starting to take this personally.")
         elseif cancelCounter > 2
             msgbox('I need some coffee.')
             return; 
@@ -33,9 +33,9 @@ function FreePlotDigitizer()
     hold on;
 
     
-    % Axes format options (tried to make it easy to add more). On another
-    % note...I'm not entirely sure why there's a select all button...but
-    % I wouldn't click it.
+    %%% Axes format options (tried to make it easy to add more). On another
+    %%% note...I'm not entirely sure why there's a select all button...but
+    %%% I wouldn't click it.
     scaleOptions = {'Linear','Log10','Reciprocal'};
     [xScaleIdx, okX] = listdlg('PromptString','X-axis scale:','ListString',scaleOptions);
     if ~okX
@@ -49,30 +49,76 @@ function FreePlotDigitizer()
     yScale = scaleOptions{yScaleIdx};
 
 
-    % Now we gotta calibrate dem axes.
-    title('Click two points on the x-axis (min to max)');
-    [px, ~] = ginput(2);
-    xInput = inputdlg({'x-axis [min max]'}, 'x calibration',1,{'0 1'});
-    if isempty(xInput)
-        return;
-    end
-    Xvals = sscanf(xInput{1}, '%f %f');
-    title('You know the drill, Same thing, but for the y-axis.');
-    [~, py] = ginput(2);
-    yInput = inputdlg({'y-axis [min to max]'}, 'Y calibration',1,{'0 1'});
-    if isempty(yInput)
-        return;
-    end
-    Yvals = sscanf(yInput{1}, '%f %f');
+   %%% Now we gotta calibrate them axes.
+    while true
+        title('Click two points on the x-axis (min to max)');
+       
+        % Get figure and axis handles
+        fig = gcf;
+        ax = gca;
+    
+        % Create line objects for the crosshairs
+        hl = line(ax, [NaN NaN], [NaN NaN], 'Color', 'm', 'LineStyle', '-', 'LineWidth', 1); % Horizontal
+        vl = line(ax, [NaN NaN], [NaN NaN], 'Color', 'm', 'LineStyle', '-', 'LineWidth', 1); % Vertical
+        set(gcf, 'Pointer', 'custom', 'PointerShapeCData', NaN(16), 'PointerShapeHotSpot', [1, 1]);
+    
+        % Attach motion callback
+        set(fig, 'WindowButtonMotionFcn', @(~,~) updateCrosshair());
+    
+        px_raw = Pylon('X-axis calibration', 2);
+        px = px_raw(:, 1);  % only take x-coordinates from pylon
+        xInput = inputdlg({'x-axis [min max]'}, 'x calibration', 1, {'0 1'});
+        if isempty(xInput)
+            return;
+        end
+        Xvals = sscanf(xInput{1}, '%f %f');
+    
+        title('You know the drill, Same thing, but for the y-axis.');
+        
+        py_raw = Pylon('Y-axis Calibration', 2);
+        py = py_raw(:, 2);   % only take y coordinates from pylon
+        yInput = inputdlg({'y-axis [min to max]'}, 'Y calibration', 1, {'0 1'});
+        if isempty(yInput)
+            return;
+        end
+        Yvals = sscanf(yInput{1}, '%f %f');
 
-    % Now time for the affiines. Unless you want to add another graph
-    % type, you can just ignore these. You'll need to figure out the affine
-    % transform for your specific graph type. If you like
-    % doing these kinds of things, my affines are translation + scale
-    % transforms...they pretty much take the form of y = mx + b.
+        userChoice = questdlg('Oi! You sure about that?', 'Axis Calibration', 'Redo', 'Continue', 'continue');
+        if strcmp(userChoice , 'Redo')
+            continue;
+        elseif strcmp(userChoice, 'Continue')
+            break;
+        else
+            break;
+        end 
+    end
+
+    % This handles the big crosshair reticles that follow your cursor
+    % around when calibrating.
+    function updateCrosshair()
+        cp = get(ax, 'CurrentPoint');
+        x = cp(1,1);
+        y = cp(1,2);
+
+        % Get axis limits
+        xlim_ = xlim(ax);
+        ylim_ = ylim(ax);
+
+        % Update line positions
+        set(hl, 'XData', xlim_, 'YData', [y y]); % Horizontal line
+        set(vl, 'XData', [x x], 'YData', ylim_); % Vertical line
+    end
+
+    %%% Now time for the affiines. Unless you want to add another graph
+    %%% type, you can just ignore these. You'll need to figure out the affine
+    %%% transform for your specific graph type. If you like
+    %%% doing these kinds of things, my affines are translation + scale
+    %%% transforms...they pretty much take the form of y = mx + b.
+    
     getLinear = @(v,vData,p) (diff(vData)./diff(p)).*(v-p(1))+vData(1);
     getLog = @(v,vData,p) 10.^(((v-p(1)).*(log10(vData(2))-log10(vData(1)))./diff(p))+log10(vData(1)));
     getRecip = @(v,vData,p) 1./(((v-p(1)).*(1./vData(2)-1./vData(1))./diff(p))+1./vData(1));
+    
     switch xScale
         case 'Linear', mapX = @(v) getLinear(v,Xvals,px);
         case 'Log10', mapX = @(v) getLog(v, Xvals,px);
@@ -84,7 +130,7 @@ function FreePlotDigitizer()
         case 'Reciprocal', mapY = @(v) getRecip(v, Yvals, py);
     end
 
-    % Now time for the hard parts. Digitization parameters first
+    %%% Now time for the hard parts. Digitization parameters first
     x_pixel = []; y_pixel = []; h = [];
     set(fig,'WindowButtonDownFcn', @click_callback);
     set(fig,'KeyPressFcn',@key_callback);
@@ -93,7 +139,8 @@ function FreePlotDigitizer()
     set(fig,'WindowButtonDownFcn','');
     set(fig,'KeyPressFcn','');
 
-    % MMMappin and savin time. This part is a bit of an eyesore
+    %%% MMMappin and savin time. This part is a bit of an eyesore.
+    
     X = mapX(x_pixel); Y = mapY(y_pixel);
     % answer = questdlg('Save as curve (CSV) or geometry (TXT)? Or you could edit this to put in your own output, I tried to make it easy.','Output format','Curve (CSV)', 'Geometry (TXT','Curve(CSV)');
     if strcmp(outputMode, 'Curve')
@@ -109,41 +156,26 @@ function FreePlotDigitizer()
             return; 
         end
         fid=fopen(fullfile(op,ofn),'w'); fprintf(fid,'# x1\tz1\tx2\tz2\tslip_rate\n');
+        
+        choice = questdlg('Oi? You want evenely spaced points across your trace?', 'Resample Geometry?', 'Yes', 'No', 'No');
 
-        % This loops over each segment. Every pair of points is treated as a segment.
-        for k=1:2:length(X)
-            prompt = {sprintf('Slip rate for the segment %df:', (k+1) / 2), 'How many points do you want in each segment?'};
-            defans = {'0', '0'};    % Default values
-            resp = inputdlg(prompt, 'Segment Parameters', 1, defans);
+        if strcmp(choice, 'Yes')
+            [X, Y] = DuctTape(X, Y);    % Mythbusters were right afterall.
+            sliprate = str2double(inputdlg({'Slip rate for all segments:'}, 'Slip Rate', 1, {'0'}));
 
-            if isempty(resp)
-                break;
-            end
+            for i = 1:length(X) - 1
+                x1 = X(i); y1 = Y(i);
+                x2 = X(i + 1); y2 = Y(i + 1);
 
-            % Casts string inputs to numerical values
-            sliprate = str2double(resp{1});    % Slip rate isn't necessary, it's just helpful to me.
-            nInterp = str2double(resp{2});
-
-            % Stores start and end points
-            x1 = X(k); x2 = X(k + 1);
-            y1 = Y(k); y2 = Y(k + 1);
-
-            % Linear interpolation between start and endpoints. Returns the amount of points the user specified in each segment.
-            for i = 0: nInterp + 1
-                t = i / (nInterp + 1);
-                xi = x1 + t * (x2 - x1);
-                yi = y1 + t * (y2 - y1);
-
-                % Saves each point found this way
-                fprintf(fid, '%.4f\t%.4f\t%.4f\t%.4f\t%.2f\n', xi, yi, xi, yi, sliprate);
+                fprintf(fid, '%.4f\t%.4f\t%.4f\t%.4f\t%.2f\n', x1, y1, x2, y2, sliprate);
             end
         end
         fclose(fid);
     end
     hold off;
 
-    % Now time for some nested callbacks. These handle user inputs within
-    % the ui. Clicking points, undoing points, etc...
+    %%% Now time for some nested callbacks. These handle user inputs within
+    %%% the ui. Clicking points, undoing points, etc...
     function click_callback(~,~)
         cp = get(gca,'CurrentPoint');
         x_pixel(end+1)=cp(1,1); y_pixel(end+1)=cp(1,2);
